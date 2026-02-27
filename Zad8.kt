249a2bc3fa3152ef2e64049c3116588c8977c6e4
Aagamir <mikimarcinkowski@gmail.com>
2026-02-27 23:15:42 +0100

    Dodane zadanie 8

---
+++ Zad8.kt
@@ -0,0 +1,16 @@
+fun countVowels(input: String): Int {
+    var count = 0
+    val vowels = mutableListOf("a", "e", "i", "o", "u", "y")
+    input.forEach{char ->
+        if (vowels.contains(char.lowercase())){
+            count++
+        }
+    }
+    return count
+}
+
+fun main(){
+    println("Podaj słowo")
+    val input = readln()
+    println("Liczba samogłosek w podanym słowie: ${countVowels(input)}")
+}
